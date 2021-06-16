8d33074f9619785b0c0200bc5070147584a70b07
Daniel Mafra <danielmafradev@gmail.com>
2021-06-16 16:51:15 -0300

    Created hook for validate form

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const types = {
+  phone: {
+    regex: /(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/,
+    message: 'Preencha um número de telefone válido.'
+  }
+}
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value)
+  }
+
+}
+
+export default useForm;
