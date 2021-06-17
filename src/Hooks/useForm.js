7f8e049aeaf102a9b5c3420eb5241f2a9c3ffdb7
Daniel Mafra <danielmafradev@gmail.com>
2021-06-16 21:48:23 -0300

    Created mask for tel

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const types = {
   phone: {
-    regex: /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/,
+    regex: /^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$/,
     message: 'Preencha um número de telefone válido.'
   }
 }
@@ -28,6 +28,12 @@ const useForm = (type) => {
   function onChange({ target }) {
     if (error) validate(target.value);
     setValue(target.value);
+    if (target.id === 'phone') {
+      const regex = /^([0-9]{2})([0-9]{4,5})([0-9]{4})$/;
+      const str = target.value.replace(/[^0-9]/g, "").slice(0, 11);
+      const result = str.replace(regex, "($1)$2-$3");
+      setValue(result);
+    }
   }
 
   return {
