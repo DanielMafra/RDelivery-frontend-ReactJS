18be24edaa9d231a994f1c49176096100232c8a6
Daniel Mafra <danielmafradev@gmail.com>
2021-07-06 17:33:50 -0300

    Add responsiveness to products

--- src/Components/Home/Product/ProductItem.js
+++ src/Components/Home/Product/ProductItem.js
@@ -6,9 +6,11 @@ const ProductItem = ({ inputCart, item }) => {
     <div className={`${styles.card} ${styles.enterLeft} ${item.isSelected && styles.disable}`} key={item.id} onClick={!item.isSelected ? () => inputCart(item) : () => false}>
       {item.offer && <div className={styles.offer}><p>Oferta</p></div>}
       <img src={item.img} alt={item.title} />
-      <h3 className={styles.title}>{item.title}</h3>
-      <p className={styles.description}>{item.description}</p>
-      <p className={styles.price}>R$ {item.price},00</p>
+      <div>
+        <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.description}>{item.description}</p>
+        <p className={styles.price}>R$ {item.price},00</p>
+      </div>
     </div>
   );
 };
