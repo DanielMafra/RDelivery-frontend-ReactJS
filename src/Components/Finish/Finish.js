6be6a2eb8832e82188948ae9a4e37e85945f99e8
Daniel Mafra <danielmafradev@gmail.com>
2021-07-06 10:28:01 -0300

    Updated finish order screen

--- src/Components/Finish/Finish.js
+++ src/Components/Finish/Finish.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { GlobalContext } from '../../GlobalContext';
+import styles from './Finish.module.css';
+import CartItem from '../Home/Cart/CartItem';
 
 const Finish = () => {
+  const { cart, total, typeBuy } = React.useContext(GlobalContext);
+
   return (
-    <div>Pedido finalizado</div>
+    <div className={styles.finishContainer}>
+      <div className={styles.orderFinish}>
+        <ion-icon name="checkmark-circle"></ion-icon>
+        <h2>Pedido finalizado</h2>
+      </div>
+      <div className={styles.orderContainer}>
+        <div className={styles.orderItems}>
+          {cart.map((item) => <CartItem key={item.id} product={item} isFinish={true} />)}
+        </div>
+        <div className={styles.total}>
+          <h4>Total</h4>
+          <h4 className={styles.price}>R$ {total},00</h4>
+        </div>
+      </div>
+      {typeBuy === "delivery" ? (
+        <p className={styles.orderStore}>Seu pedido será entregue em até 60 minutos.</p>
+      ) : (
+        <p className={styles.orderStore}>Em 25 minutos seu pedido estará pronto para ser retirado em nossa loja.</p>
+      )}
+    </div>
   )
 }
 
--- src/Components/Home/Cart/CartItem.js
+++ src/Components/Home/Cart/CartItem.js
@@ -2,22 +2,22 @@ import React from 'react';
 import styles from './CartItem.module.css';
 import { GlobalContext } from '../../../GlobalContext';
 
-const CartItem = ({ product }) => {
+const CartItem = ({ product, isFinish }) => {
   const { incrementItem, decrementItem } = React.useContext(GlobalContext);
 
   return (
     <div className={`${styles.item} ${styles.enterLeft}`}>
       <div className={styles.quantity}>
-        <button onClick={() => decrementItem(product)}>-</button>
+        {!isFinish && <button onClick={() => decrementItem(product)}>-</button>}
         <p>{product.quantity}</p>
-        <button onClick={() => incrementItem(product)}>+</button>
+        {!isFinish && <button onClick={() => incrementItem(product)}>+</button>}
       </div>
       <div className={styles.bgImage}>
         <img src={product.img} alt={product.title} />
       </div>
       <div className={styles.product}>
         <h4>{product.title}</h4>
-        <button>Adicionar observação</button>
+        {!isFinish && <button>Adicionar observação</button>}
       </div>
       <h4 className={styles.price}>R$ {product.currentPrice},00</h4>
     </div>
